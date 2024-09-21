51fb446153dd7ec734717d63d6b24f27e1cef226
Loga Naresh N <147313484+loganaresh@users.noreply.github.com>
2024-09-21 14:22:27 +0530

    project

--- recipe_project/urls.py
+++ recipe_project/urls.py
@@ -1,3 +1,4 @@
+
 from django.contrib import admin
 from django.urls import path, re_path,include
 from rest_framework import permissions
@@ -23,8 +24,8 @@ urlpatterns = [
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-    path('ma/', include('custom_auth.urls')),
-    path('ad/', include('recipes.urls')),
+    path('', include('custom_auth.urls')),
+    path('mn/', include('recipes.urls')),
 ]
 
 
